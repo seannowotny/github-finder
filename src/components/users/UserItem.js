eeade63896dc223c1350c60e261387864747855f
seannowotny <seannowotny@live.de>
2019-08-23 17:59:46 +0200

    Added single user

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+type UserProps = {};
+
+type UserState = {|
+  id: string,
+  login: string,
+  avatar_url: string,
+  html_url: string
+|};
+
+class UserItem extends Component<UserProps, UserState> {
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
+  };
+
+  render() {
+    const { login, avatar_url, html_url } = this.state;
+
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a href={html_url} className='btn btn-dar btn-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
