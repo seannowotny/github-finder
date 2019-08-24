7032366ec3f45803af2f78a5db3a52fac7734019
seannowotny <seannowotny@live.de>
2019-08-24 12:48:39 +0200

    Fetch users from GitHub API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios'; //Not an error
+
+import type { User } from './components/users/UserItem';
+
 import './App.css';
 
-class App extends Component<{}> {
+type AppProps = {};
+
+type AppState = {|
+  users: Array<User>,
+  loading: boolean
+|};
+
+type UsersResponse = {|
+  data: Array<User>
+|};
+
+class App extends Component<AppProps, AppState> {
+  state: AppState = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res: UsersResponse = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <nav className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </nav>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,46 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+
+import type { Element } from 'react';
 import type { User } from './UserItem';
 
-type UserProps = {};
-
-type UsersState = {|
-  users: Array<User>
+type UserProps = {|
+  users: Array<User>,
+  loading: boolean
 |};
 
-class Users extends Component<UserProps, UsersState> {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-      }
-    ]
-  };
-  render() {
-    return (
+const Users = ({ users, loading }: UserProps) => {
+  let result: Element<any>;
+
+  if (loading) {
+    result = <Spinner />;
+  } else {
+    result = (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+  return result;
+};
 
 const userStyle = {
   display: 'grid',
